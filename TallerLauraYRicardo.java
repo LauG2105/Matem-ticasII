7450eddd4be7ea482ff71217a6123f492d25c21f
Laura <92591619+LauG2105@users.noreply.github.com>
2023-09-15 18:00:02 -0500

    TallerLauraYRicardo.java

--- TallerLauraYRicardo.java
+++ TallerLauraYRicardo.java
@@ -1,6 +1,7 @@
 /*Como contribución al sexto objetivo de desarrollo sostenible, agua limpia y saneamiento, se crea un algoritmo capaz 
 de calcular el porcentaje semanal de personas que tuvieron acceso a agua limpia en un sector determinado
 Para esto es importante tener en cuenta que en múltiples sectores el agua limpia es suministrada a las personas a través de puntos de encuentro en un horario determinado
+Integrantes: Ricardo Rodríguez Loaiza - Laura Galvis Patiño
 */
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
